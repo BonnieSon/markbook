16e05b260eb2d7cde8042ae8f567d52068dfc042
BonnieSon <bogyung5456@gamil.com>
2021-07-07 14:11:07 +0900

    add login page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,37 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { ThemeProvider } from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core/styles';
+import SocialButton from './components/SocialButton';
+import Footer from './components/Footer';  
+
+
 
 function App() {
   const theme = createMuiTheme({
     palette: {
       primary: {
-        main: '#4D2900',
+        main: '#391F00',
+      },
+      secondary: {
+        main: '#ffffff',
       },
     },
   });
+
+ 
+
   return (
-    <div>
-     <ThemeProvider theme={theme}>
-      <Navbar />
-     </ThemeProvider>  
-    </div>
+    <>   
+      <ThemeProvider theme={theme}>
+       <Navbar />
+      </ThemeProvider>
+      <SocialButton />
+      <div>
+       <ThemeProvider theme={theme}>
+        <Footer />
+       </ThemeProvider>
+      </div> 
+  </>
   );
 }
 
