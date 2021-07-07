ff984e097724a480d8d463cf6e9197a5c4acc607
BonnieSon <bogyung5456@gamil.com>
2021-07-07 14:10:24 +0900

    repactoring code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Typography from '@material-ui/core/Typography';
+//import IconButton from '@material-ui/core/IconButton';
+//import MenuIcon from '@material-ui/icons/Menu';
+//import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 
 
@@ -14,7 +14,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(1),
   },
   blank: {
     flexGrow: 1,
@@ -29,12 +29,12 @@ export default () => {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="#ffffff">
+      <AppBar position="static" color="secondary">
         <Toolbar>   
-          <Button color="inherit">
-            <Typography color="Primary" >
+          <Button color="inherit"><img src="./images/logo.png" />
+            {/* <Typography color="Primary" >
               markbook
-            </Typography> 
+            </Typography>  */}
            </Button>
           <div className={classes.blank}>            
           </div>
