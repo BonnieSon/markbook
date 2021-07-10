d22c38b45d838129a51d4ef4a6d2d9209efcc5f4
BonnieSon <bogyung5456@gamil.com>
2021-07-10 23:40:56 +0900

    add react-router-dom, SignUpScreen page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { ThemeProvider } from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core/styles';
 import LoginScreen from './components/LoginScreen';
 import Footer from './components/Footer';  
-
+import { Switch, Route } from 'react-router-dom';
+import SignUpScreen from './components/SignUpScreen';
 
 
 function App() {
@@ -19,14 +20,19 @@ function App() {
     },
   });
 
- 
-
   return (
     <>   
       <ThemeProvider theme={theme}>
        <Navbar />
       </ThemeProvider>
-      <LoginScreen />
+      <Switch>
+        <Route exact path="/">
+          <LoginScreen />
+        </Route>
+        <Route path="/signup">
+          <SignUpScreen />
+        </Route>
+      </Switch>
       <ThemeProvider theme={theme}>
        <Footer />
       </ThemeProvider> 
