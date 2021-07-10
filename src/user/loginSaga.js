dc5153e3a438145a32395aa1c2d2605b7b3fb2fe
BonnieSon <bogyung5456@gamil.com>
2021-07-10 21:03:57 +0900

    로그인 시 user 화면 이동 코드 추가

--- src/user/loginSaga.js
+++ src/user/loginSaga.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
-import { all, call, fork, put, take, takeEvery } from 'redux-saga/effects'
+import { all, call, fork, getContext, put, take, takeEvery } from 'redux-saga/effects'
 import { reduxSagaFirebase as rsf} from '../config/reduxSagaFirebase';
 import { createAction } from '@reduxjs/toolkit';
 
@@ -9,6 +9,7 @@ const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 export const GOOGLE_LOGIN = createAction('GOOGLE_LOGIN');
 export const FACEBOOK_LOGIN = createAction('FACEBOOK_LOGIN');
 export const APPLE_LOGIN = createAction('APPLE_LOGIN');
+export const LOGOUT = createAction('LOGOUT');
 
 function* googleLogin() {
   try {
@@ -31,19 +32,31 @@ function* logout() {
 function* loginStatusWatcher() {
   // events on this channel fire when the user logs in or logs out
   const channel = yield call(rsf.auth.channel);
+  const history = yield getContext('history');
 
   while (true) {
-    const { user } = yield take(channel);
-
-    // if (user) yield put(loginSuccess(user))
-    // else yield put(logoutSuccess())
+    const { user } = yield take(channel); // 로그인시 user객체 로그아웃시 null을 반환한다.
+    //console.log(`user ${user}`);
+    if (user) {
+      const snapshot = yield call(rsf.firestore.getDocument, `users/${user.uid}`);
+      const serviceUser = snapshot.data(); // 없으면 null이 리턴 
+      history.push('/mainpage');
+      if (serviceUser === null){
+      history.push('/signup');
+     }
+      } 
+    }
   }
+
+function* getDocument() {
+  const snapshot = yield call(rsf.firestore.getDocument, `users/${user.uid}`);
+  const user = snapshot.data(); // 없으면 null이 리턴 
 }
 
 export default function* loginRootSaga() {
-  yield fork(loginStatusWatcher)
+  yield fork(loginStatusWatcher);
   yield all([
-    takeEvery (googleLogin),
-    takeEvery(logout),
+    takeEvery (GOOGLE_LOGIN.type, googleLogin),
+    takeEvery(LOGOUT.type, logout),
   ])
 }
