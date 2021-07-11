4c03d69d847871a2b5bae6bac74b1042d6b151a7
BonnieSon <bogyung5456@gamil.com>
2021-07-12 02:11:09 +0900

    signUpScreen page 추가

--- src/components/SignUpScreen.jsx
+++ src/components/SignUpScreen.jsx
@@ -1,5 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import SignUp, { BasicTextFields, RadioButtonsGroup, CheckboxLabels, ImageAvatars, UploadButtons } from './SignUp';
 
-const SignUpScreen = (props) => <h1>SignUP</h1>;
+const useStyles = makeStyles((theme) => ({
+    title: {
+        width: '60%',
+        margin: theme.spacing(10, 'auto'),
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+    }    
+        
+}));
+
+const SignUpScreen = () =>{
+    const [Id, setId] = useState('');
+    const [value, setValue] = useState('female');
+    const [state, setState] = useState({
+    checkedA: false,
+        checkedB: false,
+        checkedC: false,
+        checkedD: false,
+        checkedE: false,
+    });
+
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    // // Coustom Hook 이전
+    // const onChangeId = (e) => {
+    //     setId(e.target.value);
+    // };
+    // const onChangeTerm = (e) => {
+    //     //체크박스 초기화
+    //     setTermError(false);
+    //     setTerm(e.target.checked);
+    // }
+
+    const classes = useStyles();
+
+    return (
+      <>  
+        <h1 className={classes.title}>markbook 회원가입을 환영합니다.</h1>
+        <form onSubmit={onSubmit} style={{marginLeft:'32rem'}}>
+                <BasicTextFields />
+                <RadioButtonsGroup />
+                <CheckboxLabels />
+                <ImageAvatars />
+                <UploadButtons htmlType="submit" />
+        </form>
+      </>  
+    );
+};
 
 export default SignUpScreen;
