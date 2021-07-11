a8bd74502059de1a696b5cccf11f45d97df54c56
BonnieSon <bogyung5456@gamil.com>
2021-07-12 01:59:19 +0900

    root, toolbar에 css 코드 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,23 @@ import Typography from '@material-ui/core/Typography';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    width:'100%',
+    height: '100px',
+    position: 'absolute',
+    bottom: 0,
+    textAlign: 'center',
+    color: '#ffffff',
   },
-   appBar: {
+  appBar: {
     margin: theme.spacing(80, 0, 0, 0),
     content: '3rem',
-   },
+  },
+  toolBar: {
+    minHeight: '120px',
+  },
   text: {
     margin: theme.spacing('1rem', 'auto'),
+    color: '#ffffff',
   },
   copy: {
     padding: '1rem',
@@ -28,9 +38,9 @@ export default () => {
   return (
     <footer className={classes.root}>
       <AppBar position="absolute" backgroundcolor="primary" className={classes.appBar} >
-        <Toolbar>   
+        <Toolbar className={classes.toolBar}>   
           <img src="./images/footer_logo.png" />
-            <Typography color="secondary" className={classes.text}>
+            <Typography className={classes.text}>
               저희 markbook사이트에서는 합리적인 독서를 위해 목차별 리뷰를 남길 수 있습니다.<br />
               <span className={classes.copy}>Copyright 2021 &copy; All rights reserved. bogyung Son | bbonnie8989@gmail.com</span>              
             </Typography>
