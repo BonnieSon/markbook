685d926b1a86e3c7498182852aa00783ceceaf17
BonnieSon <bogyung5456@gamil.com>
2021-07-13 14:34:01 +0900

    firebaseApp 코드 정리

--- src/config/reduxSagaFirebase.js
+++ src/config/reduxSagaFirebase.js
@@ -3,7 +3,7 @@ import firebase from 'firebase'
 import '@firebase/firestore' // 👈 If you're using firestore
 import ReduxSagaFirebase from 'redux-saga-firebase'
 
-const firebaseApp = firebase.initializeApp(firebaseConf);
+export const firebaseApp = firebase.initializeApp(firebaseConf);
 const firebaseAuth = firebaseApp.auth();
 
 export const reduxSagaFirebase = new ReduxSagaFirebase(firebaseApp);
