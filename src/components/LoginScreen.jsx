db1ce437451f88eb89c45515a1a1c3fed93ac92d
BonnieSon <bogyung5456@gamil.com>
2021-07-14 03:58:01 +0900

    css 코드 정리 & navbar, footer 컴포넌트를 LoginScreen에 import함.

--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -7,62 +7,68 @@ import {
 import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch } from 'react-redux';
 import { GOOGLE_LOGIN, FACEBOOK_LOGIN, APPLE_LOGIN } from '../user/loginSaga';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-  },
+    backgroundColor:'#FAEBD7',
+    margin: theme.spacing(7,50),
+    borderRadius: theme.spacing(3),
+    
+    },
   title: {
     textAlign: 'center',
-    padding: theme.spacing(7, 0),
+    marginBottom: 0,
+    padding: theme.spacing(7, 0, 4, 0),
   },
   login: {
-    width: '30%',
-    // margin: 'auto',
-    margin: theme.spacing(5, 'auto'),
+    width: '40%',
+    margin: theme.spacing(0, 'auto'),
+    padding: theme.spacing(3, 0, 7, 0),
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'column',
-    // textAlign: 'center',
-    
+    flexDirection: 'column',    
   },
   facebook: {
-    width:'60%',
+    width:'80%',
     marginBottom: '1rem',
     textAlign: 'center',
-    // margin: '0',
   },
   google: {
-    width:'60%',
+    width:'80%',
     marginBottom: '1rem',
   },
   apple: {
-    width:'60%',
+    width:'80%',
+
   },
-
 }));
-
 
 export default () => {
   const classes = useStyles();
   const dispatch = useDispatch();
 
   return (
-    <>  
-      <h1 className={classes.title}>markbook 로그인</h1>
-      <div className={classes.login}>
-        <div className={classes.facebook}>
-          <FacebookLoginButton text="facebook 연결하기" onClick={() => dispatch(FACEBOOK_LOGIN())} />
-        </div>   
-        <div className={classes.google}>
-          <GoogleLoginButton text="Google 연결하기" onClick={() => dispatch(GOOGLE_LOGIN())} />
-        </div> 
-        <div className={classes.apple}>
-          <AppleLoginButton text="Apple 연결하기" onClick={() => dispatch(APPLE_LOGIN())} />
-        </div> 
+    <>
+      <Navbar />
+      <div className={classes.root}> 
+        <h1 className={classes.title}>markbook 로그인</h1>
+        <div className={classes.login}>
+          <div className={classes.facebook}>
+            <FacebookLoginButton text="facebook 연결하기" onClick={() => dispatch(FACEBOOK_LOGIN())} />
+          </div>   
+          <div className={classes.google}>
+            <GoogleLoginButton text="Google 연결하기" onClick={() => dispatch(GOOGLE_LOGIN())} />
+          </div> 
+          <div className={classes.apple}>
+            <AppleLoginButton text="Apple 연결하기" onClick={() => dispatch(APPLE_LOGIN())} />
+          </div> 
+        </div>
       </div>
-    </>  
+      <Footer /> 
+    </> 
   );
 }
