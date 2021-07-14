95aea2a180d322a73374efa79de42b91f012f48d
BonnieSon <bogyung5456@gamil.com>
2021-07-14 13:45:06 +0900

    signUp styles 코드 삭제, 폼 상태관리를 위해 각 컴포넌트에 name 추가

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
@@ -11,70 +10,7 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      '*': {
-        color: 'red',
-      },
-      margin: theme.spacing(1),
-      display: 'flex',
-      justifyContent: 'center',
-      flexDirection: 'column',
-    },
-    text: {
-      paddingBottom: '1rem',
-      marginTop: '1rem',
-      fontWeight: 'bold',
-      },
-    birth: {
-      paddingRight: '3rem',
-    },
-    idText: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '22ch',
-        display: 'flex',
-        flexDirection: 'column',
-        },
-    },
-    container: {
-        width: '50%',
-        margin: theme.spacing(1),
-        width: '25ch',
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: 200,
-    },
-    gender: {
-      display: 'inline',
-    },
-    avatar: {
-      width: theme.spacing(7),
-      height: theme.spacing(7),
-      display: 'flex',
-    },
-    input: {
-      display: 'none',
-    },
-    fileBtn: {
-      margin: theme.spacing(3, 'auto'),
-      display: 'flex',
-      flexDirection: 'row',
-    },
-    upLoadBtn: {
-      marginRight: '2rem',
-    },
-    signUpBtn: {
-      marginTop: '6rem',
-      padding: theme.spacing(2, 20),
-      fontSize: '20px',
-    },
-}));
+import { useStyles } from './SignUpStyles';
 
 
 export function BasicTextFields() {
@@ -84,15 +20,16 @@ export function BasicTextFields() {
       <>
       <div className={classes.root}> 
           <Typography className={classes.text}>
-            아이디*
+            닉네임*
           </Typography>
-          <TextField  id="standard-basic"label="ID" noValidate autoComplete="off" className={classes.idText} />
+          <TextField  name="nickName" id="standard-basic" label="닉네임" noValidate autoComplete="off" className={classes.idText} />
       </div>    
       <div className={classes.root}>  
           <Typography className={classes.text}>
             생년월일*
           </Typography>
             <TextField
+                name="birthday"
                 id="date"
                 type="date"
                 defaultValue="2017-05-24"
@@ -108,18 +45,13 @@ export function BasicTextFields() {
 
 export function RadioButtonsGroup() {
     const classes = useStyles();
-    const [value, setValue] = React.useState('female');
-  
-    const handleChange = (event) => {
-      setValue(event.target.value);
-    };
-  
+    
     return (
       <div className={classes.root}>
         <Typography className={classes.text}>
           성별*
         </Typography>
-        <RadioGroup className={classes.gender} aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+        <RadioGroup className={classes.gender} aria-label="gender" name="gender">
           <FormControlLabel value="female" control={<Radio size="small" color="#C4C4C4" />} label="여성" />
           <FormControlLabel value="male" control={<Radio size="small" color="#C4C4C4" />} label="남성" />
         </RadioGroup>
@@ -128,43 +60,32 @@ export function RadioButtonsGroup() {
 }
 
 export function CheckboxLabels() {
-    const classes = useStyles();
-    const [state, setState] = React.useState({
-      checkedA: false,
-      checkedB: false,
-      checkedC: false,
-      checkedD: false,
-      checkedE: false,
-     });
-  
-    const handleChange = (event) => {
-      setState({ ...state, [event.target.name]: event.target.checked });
-    };
+  const classes = useStyles();
   
     return (
       <div className={classes.root}>
         <Typography className={classes.text}>
           관심분야*
         </Typography>
-        <FormGroup row>
+        <FormGroup row className={classes.category}>
           <FormControlLabel
-            control={<Checkbox checked={state.checkedA} onChange={handleChange} name="checkedA" />}
+            control={<Checkbox  name="categoryBusiness" color="primary" value="Business" />}
             label="경영/경제"
           />
           <FormControlLabel
-            control={<Checkbox checked={state.checkedB} onChange={handleChange} name="checkedB" />}
+            control={<Checkbox  name="categoryImprovment" color="primary" value="Improvment" />}
             label="자기계발"
           />
           <FormControlLabel
-            control={<Checkbox checked={state.checkedC} onChange={handleChange} name="checkedC" />}
+            control={<Checkbox name="categoryNovel" color="primary" value="Novel" />}
             label="소설"
           />
           <FormControlLabel
-            control={<Checkbox checked={state.checkedD} onChange={handleChange} name="checkedD" />}
+            control={<Checkbox  name="categoryHistory" color="primary" value="History" />}
             label="역사"
           />
           <FormControlLabel
-            control={<Checkbox checked={state.checkedE} onChange={handleChange} name="checkedE" />}
+            control={<Checkbox  name="categoryScience" color="primary" value="Science" />}
             label="과학"
           />                        
         </FormGroup>
@@ -192,6 +113,7 @@ export function UploadButtons() {
     <div>
       <div className={classes.fileBtn}>
         <input
+          name="avatarImg"
           accept="image/*"
           className={classes.input}
           id="contained-button-file"
@@ -206,25 +128,12 @@ export function UploadButtons() {
           <Button
           variant="contained"
           color="gray"
-        className={classes.delBtn}
+          className={classes.delBtn}
           startIcon={<DeleteIcon />}
         >
         파일삭제
       </Button>
       </div>
-      <div>
-      <Button className={classes.signUpBtn} variant="contained" href="#contained-buttons">회원가입</Button>
-      </div>
     </div>
   );
 }
-
-// export function ContainedButtons() {
-//   const classes = useStyles();
-
-//   return (
-//     <div className={classes.root}>
-//       <Button className={classes.signUpBtn} variant="contained" href="#contained-buttons">회원가입</Button>
-//     </div>
-//   );
-// }
