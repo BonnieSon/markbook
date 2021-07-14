00e03405601eb25e32d62995cc0176a9004190d9
BonnieSon <bogyung5456@gamil.com>
2021-07-14 15:44:08 +0900

    conf 변경 수정

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const jsonServer = require('json-server');
 const admin = require('firebase-admin');
+const firebaseConf = require('./firebaseConf.json');
 const server = jsonServer.create();
 const router = jsonServer.router('server/db.json');
 const middlewares = jsonServer.defaults();
@@ -15,15 +16,7 @@ server.get('/echo', (req, res) => {
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 
-const firebaseApp = admin.initializeApp({
-  apiKey: "AIzaSyC4sa40-SmiAa3C6DYJGPvzitMrKjcXPvQ",
-  authDomain: "study-94a31.firebaseapp.com",
-  projectId: "study-94a31",
-  storageBucket: "study-94a31.appspot.com",
-  messagingSenderId: "1061623046685",
-  appId: "1:1061623046685:web:3740d433ca156227c1dcf4",
-  measurementId: "G-ET925XS5ZY"
-});
+const firebaseApp = admin.initializeApp(firebaseConf);
 
 const firebaseAuth = firebaseApp.auth();
 
