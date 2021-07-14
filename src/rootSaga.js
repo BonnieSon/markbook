f50b992ae68ec9f79901b6823c7379f671832a79
BonnieSon <bogyung5456@gamil.com>
2021-07-14 14:38:29 +0900

    signUprootSaga 추가

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,9 +1,11 @@
-import { all, call, delay, put, takeEvery } from 'redux-saga/effects'
-import loginRootSaga from './user/loginSaga'
+import { all, call, delay, put, takeEvery } from 'redux-saga/effects';
+import loginRootSaga from './user/loginSaga';
+import signUprootSaga from './user/signUpSaga';
 
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
   yield all([
     call(loginRootSaga),
+    call(signUprootSaga),
    ])
 }
