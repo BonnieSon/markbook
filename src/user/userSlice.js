f2911e94619aa73228c09a4deb52abcfc57046d0
BonnieSon <bogyung5456@gamil.com>
2021-07-16 19:02:36 +0900

    userSlice 코드 추가

--- src/user/userSlice.js
+++ src/user/userSlice.js
@@ -1,1 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user : null,    
+  },
+  reducers: {
+    loginSuccess: (state, action) {
+      state.user = action.payload;
+    },
+  }
+})
+
+/*
+const a = {
+  b : "c"
+};
+
+const b = a.b;
+const { b } = a;
+*/
+// export const loginSucces = userSlice.actions.loginSuccess;
+export const { loginSuccess } = userSlice.actions;
+export default userSlice.reducer;
+
+// const loginSuccess = createAction('LOGIN_SUCCESS');
+/*
+function reducer (initialState, action){
+  switch(action.type  === 'LOGIN_SUCCESS'){
+    state.user = action.payload;
+  }
+}
+
+yield put(loginSuccess(user))
+dispatch(loginSuccess(user));
+*/
