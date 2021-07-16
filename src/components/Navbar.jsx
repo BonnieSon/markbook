60a91022a919d13d9f6d8bf600ba8f1f1c47a984
BonnieSon <bogyung5456@gamil.com>
2021-07-16 18:57:36 +0900

    mainPage에서 변경 될 navbar 컴포넌트 코드 추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,44 +3,85 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-//import IconButton from '@material-ui/core/IconButton';
-//import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
+import { useSelector } from 'react-redux';
+import SearchIcon from '@material-ui/icons/Search';
+import serviceUser from '../user/loginSaga';
 
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  header: {
     flexGrow: 1,
-  },
-  
+    margin: theme.spacing(0, 'auto'),
+  }, 
   blank: {
     flexGrow: 1,
   },
   login: {
     marginRight: theme.spacing(2),
   },
+  search: {
+    //flexGrow: 1,
+    marginRight:'21rem',
+    marginLeft: '20rem',
+    position: 'relative',
+    border: theme.spacing(0.1, 'solid', '#D9D9D9'),
+    borderRadius: theme.spacing(0.5),
+    alignItems: 'center',
+  },
+  searchInput: {
+    width: '22rem',
+    height: '100%',
+    fontSize: '1.3rem',
+    position: 'relative',
+    border: 'none',
+    outline: 'none',
+    padding: 0,
+  },
+  searchBtn: {
+    height: '100%',
+    padding: theme.spacing(0.5, 0.7, 0.7),
+    margin: theme.spacing(0, 'auto'),
+    border: 'none',
+    cursor: 'pointer',
+    display: 'absolute',
+    borderRadius: theme.spacing(0.5),
+  },
+  logout: {
+    marginRight: '1rem',
+  },
+  avatar: {
+    //marginLeft: '3rem',
+
+  }
 }));
 
 export default () => {
   const classes = useStyles();
+  const user = useSelector(state => state.user);
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color="secondary">
-        <Toolbar>   
-          {/* <Button color="inherit" className={classes.logoBtn}><img src="./images/logo.png" /> */}
+      <AppBar position="static" color="secondary" className={classes.header}>
+        <Toolbar className={classes.inner}>   
             <Typography>
-              <a href="#" className={classes.logo}><img src="./images/logo.png" alt="logo" /></a>
-            </Typography> 
-           {/* </Button> */}
-          <div className={classes.blank}>            
-          </div>
-          <Button variant="outlined" className={classes.login}>Login</Button>
-          <Avatar />
+              <a href="#" className={classes.logo}><img src="./images/logo.png" alt="markbook" /></a>
+            </Typography>
+            <div className={classes.blank}></div>
+              { (user) ? ( <div className={classes.search}>
+              <input type="search" name="search" placeholder="제목, 저자, 출판사 검색" className={classes.searchInput} /><button className={classes.searchBtn}><SearchIcon /></button>             
+            </div>) : <div></div> }
+            { (user) ? <Button variant="outlined" className={classes.logout}>Logout</Button> : ( <Button variant="outlined" className={classes.login}>Login</Button> )}
+          { (user) ? <div><Avatar className={classes.avatar} /></div> : <Avatar />}
         </Toolbar>
       </AppBar>
-    </div>
   );
 }
 
+
+// {
+//   nickName :
+//   gender :
+//   category : [business],
+//   fileImage : file
+// }
