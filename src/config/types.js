c4e04ec1bf984cf3c04e7e7d9b7fb872ef50c709
godongs <asd236678@gmail.com>
2021-07-18 14:10:26 +0900

    types 추가

---
+++ src/config/types.js
@@ -0,0 +1,10 @@
+
+/**
+ * @typedef {object} User 
+ * @property {string} id - The firebase id
+ * @property {string} nickName - The nickName of the user
+ * @property {string} birthday - The birthday of the user
+ * @property {string} gender - The gender of the user [m, w]
+ * @property {string[]} likeCategories - like categories of the user
+ * @property {string} avatarImg - the avatorImage url of the user
+ **/ 
