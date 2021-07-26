6ae981d472c8e518628d2f9dc024ac2bf6f9cf44
godongs <asd236678@gmail.com>
2021-07-26 16:29:41 +0900

    Add Swiper functions

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -18,6 +18,8 @@ import "swiper/components/pagination/pagination.scss"; // *
 import "swiper/components/scrollbar/scrollbar.scss"; // *
 
 
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]); 
+
 
 
 const useStyles = makeStyles((theme) => ({
